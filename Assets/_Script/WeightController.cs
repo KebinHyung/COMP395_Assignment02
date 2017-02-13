aedf9e0c941ad45330c36bf9591919b5054cacb7
Kevin Luu <kevin_luu_naruto@hotmail.com>
2017-02-12 22:36:51 -0500

    First Commit

---
+++ Assets/_Script/WeightController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightController : MonoBehaviour {
+    private float positionx;
+    private float positiony;
+    public float mass;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        mass = 
+
+        positionx = Input.mousePosition.x;
+        positiony = Input.mousePosition.y;
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mouseposition = new Vector3(positionx, positiony,20.0f);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mouseposition);
+        transform.position = new Vector3(0, transform.position.y, transform.position.z);
+
+        transform.position = objPosition;
+    }
+}
